Fixed, allows for specifying number of layers and units for network.

psopy/models.py
@@ -31,7 +31,6 @@ class LinearInputsSpiralClassifier(nn.Module):
 
 
 class GenericSpiralClassifier(nn.Module):
-    layers = []
 
     def __init__(self, network_structure):
 
@@ -39,8 +38,7 @@ class GenericSpiralClassifier(nn.Module):
 
         network_structure = list(filter(lambda a: a != 0, network_structure))
 
-        # self.dimension = network_structure[0]*network_structure[1] + network_structure[1]*network_structure[2]
-
+        self.layers = nn.ModuleList()
         for i in range(len(network_structure) - 1):
             self.layers.append(make_layer(network_structure[i], network_structure[i + 1]))
 

psopy/optimization.py
@@ -8,20 +8,21 @@ class PSO(Optimizer):
     """Pytorch implementation of PSO algorithm
     """
 
-    def __init__(self, features, labels, model, loss, inertia, a1, a2, population_size, search_range, seed, cuda=False):
+    def __init__(self, features, labels, model, loss, inertia, a1, a2, population_size, search_range, dim, seed, cuda=False):
         self.features = features
         self.labels = labels
         self.model = model
         self.loss = loss
+
         self.inertia = inertia
         self.a1 = a1
         self.a2 = a2
         self.population_size = population_size
         self.search_range = search_range
-        self.cuda = cuda
-        self.seed = seed
 
-        self.dim = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        self.dim = dim
+        self.seed = seed
+        self.cuda = cuda
 
         self.positions = np.random.uniform(low=-search_range, high=search_range, size=(self.population_size, self.dim))
         self.velocities = np.random.uniform(low=-0.1, high=0.1, size=(self.population_size, self.dim))

psopy/training.py
@@ -3,9 +3,16 @@ from pathlib import Path
 
 import pandas as pd
 import torch
-from torch.nn import BCEWithLogitsLoss
+from torch.nn import BCEWithLogitsLoss, BCELoss
 
 from psopy.models import GenericSpiralClassifier
+from psopy.optimization import PSO
+
+
+def _get_accuracy(y_train, y_train_preds):
+    class_classified = (y_train_preds > 0.5).float()
+    accuracy = sum(y_train[i] == class_classified[i] for i in range(len(class_classified))) / y_train_preds.shape[0]
+    return accuracy
 
 
 class TrainingInstance:
@@ -13,74 +20,75 @@ class TrainingInstance:
     Assumes all inputs have well defined hashing functions if wanting to use the cache.
     """
 
-    def __init__(self, network_layers, optimizer, epochs, cache_loc=Path("/home/mclancy/Documents/notes/edinburgh/year4/naturalcomputing/coursework/data/cache/"), device="cpu",):
+    def __init__(self, x_train, y_train, network_layers, inertia, a1, a2, population_size, search_space, epochs, seed, x_val=None, y_val=None, cache_loc=Path("/home/mclancy/Documents/notes/edinburgh/year4/naturalcomputing/coursework/data/cache/"), device="cpu", verbose=False):
+
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_val = x_val
+        self.y_val = y_val
 
         self.network_layers = network_layers
         self.model = GenericSpiralClassifier(self.network_layers).to(device)
         self.loss = BCEWithLogitsLoss().to(device)
-        self.optimizer = optimizer
+        self.optimizer = PSO(x_train, y_train, model=self.model, loss=self.loss, inertia=inertia, a1=a1, a2=a2, population_size=population_size, search_range=search_space, seed=seed, dim=self.get_n_trainable_params())
         self.epochs = epochs
         self.device = device
-        self.trained = False
 
         self.performance_cache = cache_loc / f"performances/{hash(self)}"
         self.model_cache = cache_loc / f"models/{hash(self)}"
 
+        self.verbose = verbose
+
         if os.path.exists(self.performance_cache):
-            self._performances = self._load_cached_performances()
+            self.performances = self._load_cached_performances()
             self._load_cached_model_state()
-            self.trained = True
             print("Loaded from disk.")
+        else:
+            self.performances = pd.DataFrame(index=pd.MultiIndex.from_product([["train", "val"], ["fitness", "accuracy"]]),
+                                              columns=[i for i in range(self.epochs)]
+                                              )
+            self._fit()
 
-
-    def fit(self, x_train, y_train, x_val=None, y_val=None, verbose=False):
-        if self.trained is True:
-            raise Exception("Already trained")
-
-        performances = pd.DataFrame(index=pd.MultiIndex.from_product([["train", "val"], ["fitness", "accuracy"]]),
-                                    columns=[i for i in range(self.epochs)]
-                                    )
-
+    def _fit(self):
         for i in range(self.epochs):
-            y_train_preds = self.model(x_train)
-            fitness = self.loss(y_train_preds, y_train)
-            performances.loc[("train", "fitness")][i] = fitness
+            y_train_preds = self.model(self.x_train)
+            fitness = self.loss(y_train_preds, self.y_train)
+            self.performances.loc[("train", "fitness")][i] = fitness
 
-            accuracy = self._get_accuracy(y_train, y_train_preds)
-            performances.loc[("train", "accuracy")][i] = accuracy
 
-            if verbose:
+            accuracy = _get_accuracy(self.y_train, y_train_preds)
+            self.performances.loc[("train", "accuracy")][i] = accuracy
+
+            if self.verbose:
                 print(f"Epoch {i}: Fitness = {fitness}; Training Acc = {accuracy}")
 
-            if x_val is not None and y_val is not None:
-                y_val_preds = self.model(x_val)
-                val_fitness = self.loss(y_val_preds, y_val)
-                val_acc = self._get_accuracy(y_val, y_val_preds)
-                performances.loc[("val", "fitness")][i] = val_fitness
-                performances.loc[("val", "accuracy")][i] = val_acc
+            if self.x_val is not None and self.y_val is not None:
+                y_val_preds = self.model(self.x_val)
+                val_fitness = self.loss(y_val_preds, self.y_val)
+                val_acc = _get_accuracy(self.y_val, y_val_preds)
+                self.performances.loc[("val", "fitness")][i] = val_fitness
+                self.performances.loc[("val", "accuracy")][i] = val_acc
 
             self.optimizer.step()
 
-        self.trained = True
-        self._performances = performances
         self._cache_performances()
         self._cache_model_state()
 
-    @property
-    def performances(self):
-        assert self.trained is True
-        return self._performances
+    def get_n_trainable_params(self):
+        n = 0
+        for name, param in self.model.named_parameters():
+            if any(x in name for x in ["weight", "bias"]):
+                n += param.numel()
+        return n
 
-    def _get_accuracy(self, y_train, y_train_preds):
-        class_classified = (y_train_preds > 0.5).float()
-        accuracy = sum(y_train[i] == class_classified[i] for i in range(len(class_classified))) / y_train_preds.shape[0]
-        return accuracy
+    def get_current_performances(self):
+        return self.performances.iloc[:, -1:]
 
     def _load_cached_performances(self):
         return pd.read_hdf(self.performance_cache, key="accuracies")
     
     def _cache_performances(self):
-        self._performances.to_hdf(self.performance_cache, key="accuracies")
+        self.performances.to_hdf(self.performance_cache, key="accuracies")
 
     def _load_cached_model_state(self):
         self.model.load_state_dict(torch.load(self.model_cache))
@@ -93,3 +101,4 @@ class TrainingInstance:
 
     def __hash__(self):
         return hash((self.epochs, self._get_model_hash(), hash(self.optimizer), self.device))
+